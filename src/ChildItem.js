33715e004012b577fdc4897290a6a89614019143
Marcos-fa <marcos.fa0216@gmail.com>
2022-12-21 12:55:04 -0700

    modified to work with app

--- src/ChildItem.js
+++ src/ChildItem.js
@@ -1,5 +1,9 @@
-import React from 'react';
-import {TouchableOpacity, Image, StyleSheet} from 'react-native';
+import React, { useContext } from 'react';
+import { Pressable, Image, View, Text } from 'react-native';
+import MaskedView from '@react-native-masked-view/masked-view';
+import LinearGradient from 'react-native-linear-gradient';
+import { ThemeContext } from '../../../context/theme.context';
+
 
 export default (ChildItem = ({
   item,
@@ -8,22 +12,32 @@ export default (ChildItem = ({
   index,
   imageKey,
   local,
-  height,
 }) => {
-  return (
-    <TouchableOpacity style={styles.container} onPress={() => onPress(index)}>
-      <Image
-        style={[styles.image, style, {height: height}]}
-        source={local ? item[imageKey] : {uri: item[imageKey]}}
-      />
-    </TouchableOpacity>
-  );
-});
 
-const styles = StyleSheet.create({
-  container: {},
-  image: {
-    height: 230,
-    resizeMode: 'stretch',
-  },
+  const { Main, theme } = useContext(ThemeContext);
+  return (
+    <Pressable
+      style={[Main.containerLib, style]}
+      onPress={() => onPress(index)}>
+      <View style={{ paddingTop: 20 }}>
+        <MaskedView maskElement={<Text style={Main.titleLib} >{item["title"]}</Text>}>
+          <LinearGradient
+            colors={[theme.dotOffColor, theme.dotColor]}
+            start={{ x: 0, y: 0 }}
+            end={{ x: 1, y: 0 }}
+          >
+            <Text style={[Main.titleLib, { opacity: 0 }]} >{item["title"]}</Text>
+          </LinearGradient>
+        </MaskedView>
+      </View>
+
+      <View style={Main.descriptionLib}>
+        <Text style={Main.description}>{item["desc"]}</Text>
+      </View>
+
+      <View style={Main.imageLib}>
+        <Image source={local ? item[imageKey] : { uri: item[imageKey] }} />
+      </View>
+    </Pressable>
+  );
 });
